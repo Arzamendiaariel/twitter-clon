7693bc285f7734d0e0b137dfb508886a7a780f1f
arzamendiaariel <arzamendiaariel@gmail.com>
2022-03-25 21:17:00 -0300

    first commit

---
+++ pages/timeline/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const timeline = () => {
+  return (
+    <>
+      <h1>Timeline</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </>
+  );
+};
+
+export default timeline;
